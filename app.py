036210af31d6290380d8d8757e2a9ba9396ac0df
operator.name <operator.name@protonmail.com>
2023-08-07 18:38:24 +0100

    🔬 Verify expected value for multiple rounds

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import random
-from statistics import mean
+from statistics import mean, median
 
 WIN_REWARD = 0.5
 LOSE_COST = 0.4
@@ -23,11 +23,49 @@ def simulate(starting_wealth, players, rounds):
 starting_wealth = 100.0
 players = 10000
 rounds = 1
-expected_1 = 0.5 * (starting_wealth * WIN + starting_wealth * LOSE)
 
 wallets = simulate(starting_wealth, players, rounds)
-average = mean(wallets)
+avg = mean(wallets)
+exp_avg = starting_wealth * ((0.5 * (WIN + LOSE)) ** rounds)
 
+med = median(wallets)
+winners = len([w for w in wallets if w > starting_wealth])
+big_losers = len([w for w in wallets if w > (0.1 * starting_wealth)])
+
+print(f"{starting_wealth=}, {players=}, {rounds=}")
+print(f"mean = {avg}")
 print("lim players -> inf")
-print(f"expected -> {expected_1}")
-print(f"mean(simulate({starting_wealth}, {players}, {rounds})) = {average}")
+print(f"{rounds=}")
+print(f"expected mean -> {exp_avg}")
+print()
+print(f"median = {med}")
+print(f"wallets > starting_wealth = {winners/players:.0%}")
+print(f"wallets < 0.1 * starting_wealth = {big_losers/players:.0%}")
+print(f"{max(wallets)=}, {min(wallets)=}")
+
+print()
+print()
+
+rounds = 10
+
+wallets = simulate(starting_wealth, players, rounds)
+avg = mean(wallets)
+exp_avg = starting_wealth * ((0.5 * (WIN + LOSE)) ** rounds)
+
+med = median(wallets)
+winners = len([w for w in wallets if w > starting_wealth])
+big_losers = len([w for w in wallets if w > (0.1 * starting_wealth)])
+
+print(f"{starting_wealth=}, {players=}, {rounds=}")
+print(f"mean = {avg}")
+print(f"{rounds=}")
+print("lim players -> inf")
+print(f"expected mean -> {exp_avg}")
+print()
+print(f"median = {med}")
+print(f"wallets > starting_wealth = {winners/players:.0%}")
+print(f"wallets < 0.1 * starting_wealth = {big_losers/players:.0%}")
+print(f"{max(wallets)=}, {min(wallets)=}")
+
+print()
+print()
