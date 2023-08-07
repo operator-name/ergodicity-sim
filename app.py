b91ec70e38ab7a185c29a1b183ec65bd34359833
operator.name <operator.name@protonmail.com>
2023-08-07 18:38:24 +0100

    ⤴️ Extract stats printing function

--- app.py
+++ app.py
@@ -20,52 +20,38 @@ def simulate(starting_wealth, players, rounds):
     return wallets
 
 
+def print_stats(wallets):
+    avg = mean(wallets)
+    exp_avg = starting_wealth * ((0.5 * (WIN + LOSE)) ** rounds)
+
+    med = median(wallets)
+    winners = len([w for w in wallets if w > starting_wealth])
+    big_losers = len([w for w in wallets if w > (0.1 * starting_wealth)])
+
+    print(f"{starting_wealth=}, {players=}, {rounds=}")
+    print(f"mean = {avg:.2f}")
+    print("lim players -> inf")
+    print(f"expected mean -> {exp_avg:.2f}")
+    print(f"median = {med:.2f}")
+    print(f"wallets > {starting_wealth} = {winners/players:.2%}")
+    print(f"wallets < {0.1 * starting_wealth} = {big_losers/players:.2%}")
+    print(f"{max(wallets)=:.2f}, {min(wallets)=:.2f}")
+
+
 starting_wealth = 100.0
 players = 10000
 rounds = 1
 
 wallets = simulate(starting_wealth, players, rounds)
-avg = mean(wallets)
-exp_avg = starting_wealth * ((0.5 * (WIN + LOSE)) ** rounds)
-
-med = median(wallets)
-winners = len([w for w in wallets if w > starting_wealth])
-big_losers = len([w for w in wallets if w > (0.1 * starting_wealth)])
-
-print(f"{starting_wealth=}, {players=}, {rounds=}")
-print(f"mean = {avg}")
-print("lim players -> inf")
-print(f"{rounds=}")
-print(f"expected mean -> {exp_avg}")
-print()
-print(f"median = {med}")
-print(f"wallets > starting_wealth = {winners/players:.0%}")
-print(f"wallets < 0.1 * starting_wealth = {big_losers/players:.0%}")
-print(f"{max(wallets)=}, {min(wallets)=}")
-
+print_stats(wallets)
 print()
 print()
 
+starting_wealth = 100.0
+players = 10000
 rounds = 10
 
 wallets = simulate(starting_wealth, players, rounds)
-avg = mean(wallets)
-exp_avg = starting_wealth * ((0.5 * (WIN + LOSE)) ** rounds)
-
-med = median(wallets)
-winners = len([w for w in wallets if w > starting_wealth])
-big_losers = len([w for w in wallets if w > (0.1 * starting_wealth)])
-
-print(f"{starting_wealth=}, {players=}, {rounds=}")
-print(f"mean = {avg}")
-print(f"{rounds=}")
-print("lim players -> inf")
-print(f"expected mean -> {exp_avg}")
-print()
-print(f"median = {med}")
-print(f"wallets > starting_wealth = {winners/players:.0%}")
-print(f"wallets < 0.1 * starting_wealth = {big_losers/players:.0%}")
-print(f"{max(wallets)=}, {min(wallets)=}")
-
+print_stats(wallets)
 print()
 print()
